794494ad8bc935844d9e05ce3080ebbb6c04aa6f
赵李波 <83433430@qq.com>
2019-08-13 17:40:08 +0800

    no message

--- app/service/serviceCommon.ts
+++ app/service/serviceCommon.ts
@@ -24,7 +24,7 @@ export default class ExportService extends Service {
     try {
 
       //读取模板文件
-      let dir = path.resolve(__dirname, '../../docx');
+      let dir = path.resolve(__dirname, '../public/docx');
       base64_1 = base64_1.replace(/^data:image\/\w+;base64,/, "");//去掉图片base64码前面部分data:image/png;base64
       let dataBuffer = Buffer.from(base64_1); //把base64码转成buffer对象，
 
@@ -65,9 +65,9 @@ export default class ExportService extends Service {
       });
       doc.render();
       var buf = doc.getZip().generate({ type: 'nodebuffer' });
-      // fs.writeFileSync(path.join(dir, 'b.docx'), buf);
+      fs.writeFileSync(path.join(dir, 'b.docx'), buf);
       console.log('aaaaaaaaaaaaaaa11111111');
-      jResult.data = buf;
+      jResult.data = 'http://127.0.0.1:7002/docx/b.docx';
     } catch (error) {
       console.log('bbbbbbbbbbbbbbbb');
       jResult.code = 601;
